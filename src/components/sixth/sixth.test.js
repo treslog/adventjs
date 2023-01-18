8f898fc6457507fb964161ab3c9c1303100f2c12
Tristan Vidal <trsgot@gmail.com>
2023-01-17 21:23:08 -0500

    Sixth

--- src/components/sixth/sixth.test.js
+++ src/components/sixth/sixth.test.js
@@ -1,7 +1,16 @@
 import { describe, it, expect } from 'vitest'
 
+export function sumPairs() {
+    return
+}
+
 describe('sumPairs', () => {
     it('should be a function', () => {
         expect(typeof daysToXmas).toBe('function')
     })
+
+    it('throw an error if no argument is given', () => {
+        expect(() => sumPairs()).toThrow()
+    })
+
 })
